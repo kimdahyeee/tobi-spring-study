b024bf7f5fa2be6cd7b6963b255a24978eb587b7
kimdahye <dahye4321@naver.com>
2018-10-21 02:08:48 +0900

    ch3. 3.5.3

---
+++ src/main/java/com/dahye/learningtest/template/Calculator.java
@@ -0,0 +1,61 @@
+package com.dahye.learningtest.template;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Calculator {
+
+    public Integer calcSum(String filePath) throws IOException {
+        BufferedReaderCallback sumCallback = new BufferedReaderCallback() {
+            public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+                Integer sum = 0;
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    sum += Integer.valueOf(line);
+                }
+
+                return sum;
+            }
+        };
+
+        return fileReadTemplate(filePath, sumCallback);
+    }
+
+    public Integer calcMultiply(String filePath) throws IOException {
+        BufferedReaderCallback sumCallback = new BufferedReaderCallback() {
+            public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+                Integer multi = 1;
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    multi *= Integer.valueOf(line);
+                }
+
+                return multi;
+            }
+        };
+
+        return fileReadTemplate(filePath, sumCallback);
+    }
+
+    public Integer fileReadTemplate(String filePath, BufferedReaderCallback callback) throws IOException {
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new FileReader(filePath));
+            int ret = callback.doSomethingWithReader(br);
+            return ret;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            if(br!= null) {
+                try {
+                    br.close();
+                } catch (IOException e ) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+    }
+}
---
+++ src/test/java/lerningtest/template/CalcSumTest.java
@@ -0,0 +1,36 @@
+package lerningtest.template;
+
+import com.dahye.learningtest.template.Calculator;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.IOException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class CalcSumTest {
+
+    Calculator calculator;
+    String numFilepath;
+
+    @Before
+    public void setUp() {
+        this.calculator = new Calculator();
+        this.numFilepath = getClass().getResource("/numbers.txt").getPath();
+    }
+
+    @Test
+    public void sumOfNumbers() throws IOException {
+        int sum = calculator.calcSum(numFilepath);
+        assertThat(sum, is(10));
+    }
+
+    @Test
+    public void multiplyOfNumbers() throws IOException {
+        int mul = calculator.calcMultiply(numFilepath);
+        assertThat(mul, is(24));
+    }
+}
