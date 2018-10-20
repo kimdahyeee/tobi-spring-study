8436bfef99705108552efba63a71dfc0af56be35
kimdahye <dahye4321@naver.com>
2018-10-20 19:43:29 +0900

    ch2. 내용 delete

--- src/main/java/com/dahye/dao/ConnectionMaker.java
+++
@@ -1,9 +0,0 @@
-package com.dahye.dao;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionMaker {
-
-    public Connection makeConnection() throws ClassNotFoundException, SQLException;
-}
