263b0555b47ebf1c752d1268aec4de34f7f77236
kimdahye <dahye4321@naver.com>
2018-10-21 12:30:12 +0900

    ch3. 3.6.3

--- src/main/java/com/dahye/user/dao/UserDao.java
+++ src/main/java/com/dahye/user/dao/UserDao.java
@@ -8,6 +8,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
 import java.sql.*;
@@ -28,31 +29,18 @@ public class UserDao {
                 user.getId(), user.getName(), user.getPassword());
     }
 
-    public User get(String id) throws SQLException {
-        Connection c = dataSource.getConnection();
-
-        PreparedStatement ps = c.prepareStatement(
-                "select * from users where id = ?"
-        );
-        ps.setString(1, id);
-
-        ResultSet rs = ps.executeQuery();
-
-        User user = null;
-        if(rs.next()) {
-            user = new User();
-            user.setId(rs.getString("id"));
-            user.setName(rs.getString("name"));
-            user.setPassword(rs.getString("password"));
-        }
-
-        rs.close();
-        ps.close();
-        c.close();
-
-        if(user == null) throw new EmptyResultDataAccessException(1);
-
-        return user;
+    public User get(String id) {
+        return this.jdbcTemplate.queryForObject("select * from users where id = ?",
+                    new Object[]{id},
+                    new RowMapper<User>() {
+                        public User mapRow(ResultSet resultSet, int i) throws SQLException {
+                            User user = new User();
+                            user.setId(resultSet.getString("id"));
+                            user.setName(resultSet.getString("name"));
+                            user.setPassword(resultSet.getString("password"));
+                            return user;
+                        }
+                });
     }
 
     public void deleteAll() {
@@ -60,34 +48,6 @@ public class UserDao {
     }
 
     public int getCount() {
-        return this.jdbcTemplate.query(new PreparedStatementCreator() {
-            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
-                return connection.prepareStatement("select count(*) from users");
-            }
-        }, new ResultSetExtractor<Integer>() {
-            public Integer extractData(ResultSet resultSet) throws SQLException, DataAccessException {
-                resultSet.next();
-                return resultSet.getInt(1);
-            }
-        });
-    }
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        ApplicationContext context = new GenericXmlApplicationContext("classpath:applicationContext-*.xml");
-        UserDao dao = context.getBean("userDao", UserDao.class);
-
-        User user = new User();
-        user.setId("dahyekim16");
-        user.setName("김다혜");
-        user.setPassword("dahye");
-
-        dao.add(user);
-
-        System.out.println(user.getId() +" 등록 성공");
-
-        User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
-
-        System.out.println(user2.getId() + " 조회 성공");
+        return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
     }
 }
