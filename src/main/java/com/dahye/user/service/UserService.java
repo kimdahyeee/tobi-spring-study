fa2409f31b6c67155d9ae59ed6bd7bf9f7863b67
kimdahye <dahye4321@naver.com>
2018-11-13 00:55:52 +0900

    CH6.1.1 메소드 분리

--- src/main/java/com/dahye/user/service/UserService.java
+++ src/main/java/com/dahye/user/service/UserService.java
@@ -38,18 +38,22 @@ public class UserService {
         TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
 
         try {
-            List<User> users = userDao.getAll();
-
-            for (User user : users) {
-                if(canUpgradeGrade(user)) {
-                    upgradeGrade(user);
-                }
-            }
+            upgradeGradesInternal();
 
             transactionManager.commit(status);
         } catch (Exception e) {
             transactionManager.rollback(status);
             throw e;
+        }
+    }
+
+    private void upgradeGradesInternal() {
+        List<User> users = userDao.getAll();
+
+        for (User user : users) {
+            if(canUpgradeGrade(user)) {
+                upgradeGrade(user);
+            }
         }
     }
 
