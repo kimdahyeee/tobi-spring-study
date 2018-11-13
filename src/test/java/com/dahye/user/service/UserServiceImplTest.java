6ffc9512f7ee0330333fd5615cd24055470269c4
kimdahye <dahye4321@naver.com>
2018-11-13 23:16:10 +0900

    ch6.2.2 userdao mock 구현

--- src/test/java/com/dahye/user/service/UserServiceImplTest.java
+++ src/test/java/com/dahye/user/service/UserServiceImplTest.java
@@ -79,6 +79,44 @@ public class UserServiceImplTest {
         }
     }
 
+    static class MockUserDao implements UserDao {
+        private List<User> users;
+        private List<User> updated = new ArrayList<User>();
+
+        private MockUserDao(List<User> users) {
+            this.users = users;
+        }
+
+        public List<User> getUpdated() {
+            return this.updated;
+        }
+
+        public List<User> getAll() {
+            return this.users;
+        }
+
+        public void update(User user) {
+            updated.add(user);
+        }
+
+        public void add(User user) {
+            throw new UnsupportedOperationException();
+        }
+
+        public User get(String id) {
+            throw new UnsupportedOperationException();
+        }
+
+        public void deleteAll() {
+            throw new UnsupportedOperationException();
+        }
+
+        public int getCount() {
+            throw new UnsupportedOperationException();
+        }
+
+    }
+
     @Before
     public void setUp() {
         users = Arrays.asList(
@@ -93,25 +131,29 @@ public class UserServiceImplTest {
     @Test
     @DirtiesContext
     public void upgradeGrade() throws Exception {
-        userDao.deleteAll();
-
-        for(User user : users) userDao.add(user);
+        UserServiceImpl userServiceImpl = new UserServiceImpl();
+        MockUserDao mockUserDao = new MockUserDao(this.users);
+        userServiceImpl.setUserDao(mockUserDao);
 
         MockMailSender mockMailSender = new MockMailSender();
         userServiceImpl.setMailSender(mockMailSender);
 
-        userService.upgradeGrades();
+        userServiceImpl.upgradeGrades();
 
-        checkGradeUpgrade(users.get(0), false);
-        checkGradeUpgrade(users.get(1), true);
-        checkGradeUpgrade(users.get(2), false);
-        checkGradeUpgrade(users.get(3), true);
-        checkGradeUpgrade(users.get(4), false);
+        List<User> updated = mockUserDao.getUpdated();
+        assertThat(updated.size(), is(2));
+        checkUserAndGrade(updated.get(0), "dahye2", Grade.SILVER);
+        checkUserAndGrade(updated.get(1), "dahye4", Grade.GOLD);
 
         List<String> request = mockMailSender.getReq();
         assertThat(request.size(), is(2));
         assertThat(request.get(0), is(users.get(1).getEmail()));
         assertThat(request.get(1), is(users.get(3).getEmail()));
+    }
+
+    private void checkUserAndGrade(User updated, String expectedId, Grade expectedGrade) {
+        assertThat(updated.getId(), is(expectedId));
+        assertThat(updated.getGrade(), is(expectedGrade));
     }
 
     private void checkGradeUpgrade(User user, boolean changed) {
