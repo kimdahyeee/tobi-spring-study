1ac927483a959eb89e20ed1f1615f1f20c5928e9
kimdahye <dahye4321@naver.com>
2018-12-02 18:44:33 +0900

    ch6 transactional annotation...

--- src/main/java/com/dahye/user/service/UserService.java
+++ src/main/java/com/dahye/user/service/UserService.java
@@ -1,13 +1,20 @@
 package com.dahye.user.service;
 
 import com.dahye.user.domain.User;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional
 public interface UserService {
     void add(User user);
+
+    @Transactional(readOnly = true)
     User get(String id);
+
+    @Transactional(readOnly = true)
     List<User> getAll();
+
     void deleteAll();
     void update(User user);
     void upgradeGrades();
